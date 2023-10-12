cfb060495e4e03ce2ab3cc96af89f514106a4f43
anjana732 <anjanasingh1257@gmail.com>
2023-10-12 19:34:54 +0530

    Completed Registration section

--- Registration.php
+++ Registration.php
@@ -1,9 +1,26 @@
 <?php
-    print_r($_POST);
-    if(isset($_POST['submit'])){
+    $server ="localhost";
+    $user = "root";
+    $pass = "";
+
+    $con = mysqli_connect($server, $user, $pass);
+    if(!$con){
+        die("Connection to database failed due to".mysqli_connect_error());
+    }
+
         $name = $_POST['name'];
         $email = $_POST['email'];
         $username = $_POST['username'];
         $password = $_POST['password'];
-    }
+
+        $sql ="INSERT INTO `DepartmentManagement`.`Department` (`name`, `email`, `username`, `password`) VALUES ('$name', '$email', '$username', '$password');";
+        echo $sql;
+
+        if($con->query($sql)==ture){
+            echo "Successfully inserted";
+        }else{
+            echo "Error: $sql <br> $con->error";
+        }
+        $con->close();
+    
 ?>
